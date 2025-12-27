c08487d4833e78200edd4eae4a5532f092ab43cb
gopi-banjara <an3201088@gmail.com>
2025-12-27 20:24:06 +0530

    Clean up database.js by removing unused code
    Removed commented-out code for database connection.
    
    Signed-off-by: gopi-banjara <an3201088@gmail.com>

--- backend-server/src/config/database.js
+++ backend-server/src/config/database.js
@@ -13,21 +13,3 @@ db.getConnection()
     .catch(err => console.error('Database connection failed:', err));
 
 module.exports = db;
-
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("DB connection failed:", err);
-//   } else {
-//     console.log("Database connected");
-//   }
-// });
-
-module.exports = db;
